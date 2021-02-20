803bfae88afc4a9c21ee4c6d0bd013be30aa52f1
claytonjacobs <cjacobs1616@gmail.com>
2021-02-20 15:46:28 -0800

    done?

--- ui.cpp
+++ ui.cpp
@@ -9,13 +9,41 @@ class UserInterface {
     string title;
     void InitState();
   };
+
+  class Header {
+    string title;
+    void RecieveCurState();
+  };
+
+  class Body {
+    int num_floors = 1;
+    Object noise_level;
+    void RecieveCurState();
+  }
+
+  class Footer {
+    int button_navigator;
+    Object ChangeScreenState(int screen);
+  }
 };
 
-void UserInterface::StateManager::InitState() { /* Do Something */
+void UserInterface::StateManager::InitState() {
+  /* * * * * * * * */
+  /* Do Something  */
+  /* * * * * * * * */
 }
 
 int main() {
   UserInterface ui;
+
+  ui::Header h;
+  h.RecieveCurState();
+
+  ui::Body b;
+  b.RecieveCurState();
+
+  ui::Footer f;
+  f.ChangeScreenState(int 1);  // screen 1 = home screen
 
   return 0;
 }
